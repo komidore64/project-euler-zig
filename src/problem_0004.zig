d20457735fa1536eaa1a2cb96ddf85d7bdba8633
Adam Price <adam.price@validic.com>
2022-10-28 12:53:43 -0400

    solution #4

--- src/problem_0004.zig
+++ src/problem_0004.zig
@@ -2,20 +2,21 @@ const std = @import("std");
 const expect = std.testing.expect;
 
 pub fn main() !void {
-    std.debug.print("{d}\n", .{try solve(10, 99)});
+    std.debug.print("{d}\n", .{try solve(100, 999)});
 }
 
-// FIXME: this _looks_ like it should return the first discovered palindromic
-// number, but it returns `990` for some reason. debug to figure out.
-//
-// I think I'm constructing my reverse digit array incorrectly.
 fn solve(start: u32, stop: u32) !u32 {
+    var largest_product: u32 = 0;
     var factor1 = start;
-    var factor2 = stop;
 
     while (factor1 >= start and factor1 <= stop) : (factor1 += 1) {
+        var factor2 = start;
+
         while (factor2 >= start and factor2 <= stop) : (factor2 += 1) {
             const product = factor1 * factor2;
+
+            if (product < largest_product)
+                continue;
 
             var digits_array = std.ArrayList(u32).init(std.heap.page_allocator);
             defer digits_array.deinit();
@@ -24,41 +25,69 @@ fn solve(start: u32, stop: u32) !u32 {
             var reverse_digits_array = std.ArrayList(u32).init(std.heap.page_allocator);
             defer reverse_digits_array.deinit();
             for (digits_array.items) |digit| {
-                try reverse_digits_array.append(digit);
+                try reverse_digits_array.insert(0, digit);
             }
 
             if (std.mem.eql(u32, digits_array.items, reverse_digits_array.items))
-                return product;
+                largest_product = product;
         }
     }
-    unreachable;
+
+    return largest_product;
 }
 
-// test "solve" {
-//     try expect(solve(10, 99) == 9_009);
-//     try expect(solve(100, 999) == 906_609);
-// }
+test "solve" {
+    try expect(try solve(10, 99) == 9_009);
+    try expect(try solve(100, 999) == 906_609);
+}
 
 fn intToArrayList(num: u32, al: *std.ArrayList(u32)) !void {
-    var div: u32 = 1;
+    try al.insert(0, num % 10); // account for `0`
 
-    while (div < num) : (div *= 10) {
+    var div: u32 = 10;
+    while (div <= num) : (div *= 10) {
         try al.insert(0, num % (div * 10) / div);
     }
 }
 
-test "intToArrayList" {
-    var test_arr1 = std.ArrayList(u32).init(std.testing.allocator);
-    defer test_arr1.deinit();
+test "intToArrayList(1234)" {
+    var test_arr = std.ArrayList(u32).init(std.testing.allocator);
+    defer test_arr.deinit();
 
-    try intToArrayList(@as(u32, 1234), &test_arr1);
-    try expect(std.mem.eql(u32, test_arr1.items, &[_]u32{ 1, 2, 3, 4 }));
+    try intToArrayList(@as(u32, 1234), &test_arr);
+    try expect(std.mem.eql(u32, test_arr.items, &[_]u32{ 1, 2, 3, 4 }));
+}
 
-    var test_arr2 = std.ArrayList(u32).init(std.testing.allocator);
-    defer test_arr2.deinit();
+test "intToArrayList(5678)" {
+    var test_arr = std.ArrayList(u32).init(std.testing.allocator);
+    defer test_arr.deinit();
 
-    try intToArrayList(@as(u32, 5678), &test_arr2);
-    try expect(std.mem.eql(u32, test_arr2.items, &[_]u32{ 5, 6, 7, 8 }));
+    try intToArrayList(@as(u32, 5678), &test_arr);
+    try expect(std.mem.eql(u32, test_arr.items, &[_]u32{ 5, 6, 7, 8 }));
+}
+
+test "intToArrayList(100)" {
+    var test_arr = std.ArrayList(u32).init(std.testing.allocator);
+    defer test_arr.deinit();
+
+    try intToArrayList(@as(u32, 100), &test_arr);
+    try expect(std.mem.eql(u32, test_arr.items, &[_]u32{ 1, 0, 0 }));
+}
+
+test "intToArrayList(50)" {
+    var test_arr = std.ArrayList(u32).init(std.testing.allocator);
+    defer test_arr.deinit();
+
+    try intToArrayList(@as(u32, 50), &test_arr);
+    try expect(std.mem.eql(u32, test_arr.items, &[_]u32{ 5, 0 }));
+}
+
+test "intToArrayList(0)" {
+    var test_arr = std.ArrayList(u32).init(std.testing.allocator);
+    defer test_arr.deinit();
+
+    try intToArrayList(@as(u32, 0), &test_arr);
+    try expect(std.mem.eql(u32, test_arr.items, &[_]u32{0}));
 }
 
 // okay, at least i'm using `std.mem.eql()` correctly
